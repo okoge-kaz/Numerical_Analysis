3ceaaf2b9c94e15030142ddd90b04f111e63ee09
Fujii Kazuki <kaz.tokyo.tech20@gmail.com>
2022-06-10 14:55:39 +0900

    pass assignment 6

---
+++ report/6.py
@@ -0,0 +1,68 @@
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def Laplace_solver(N: int, k_max: int, eps: float, x_len: float, t_len: float) -> None:
+    dx: float = x_len / N
+    dt: float = t_len / N
+
+    X: np.ndarray = np.linspace(0, 1, N + 1)
+    for i in range(1, N + 1):
+        X[i] = X[i - 1] + dx
+
+    T: np.ndarray = np.linspace(0, 1, N + 1)
+    for t in range(1, N + 1):
+        T[t] = T[t - 1] + dt
+
+    U: np.ndarray = np.zeros((N + 1, N + 1))
+    for t in range(0, N + 1):
+        U[0, t] = 0
+        U[N, t] = 0
+    for x in range(0, N + 1):
+        U[x, 0] = (dx * x) * (1 - dx * x)
+
+    k: int = 0
+
+    while True:
+        k += 1
+        old_U: np.ndarray = np.copy(U)
+        for x in range(1, N):
+            for t in range(1, N):
+                U[x, t] = (U[x, t - 1] + U[x, t + 1] + U[x - 1, t] + U[x + 1, t]) / 4
+
+        rdelta: float = np.amax(abs((U - old_U) / U))
+        if rdelta < eps:
+            break
+        if k > k_max:
+            break
+
+    for t in range(0, N + 1):
+        if t * dt in [0.0, 0.25, 0.5, 0.75, 1.0]:
+            plt.title("Laplace equation t = " + str(t * dt))
+            plt.plot(X, U[:, t], label="t= " + str(dt * t))
+            plt.xlabel("x")
+            plt.ylabel(f"u(x, {dt * t})")
+            plt.legend()
+            plt.show()
+
+    for x in range(0, N + 1):
+        if x * dx in [0.0, 0.25, 0.5]:
+            plt.title("Laplace equation x = " + str(x * dx))
+            plt.plot(T, U[x, :], label="x= " + str(dx * x))
+            plt.xlabel("t")
+            plt.ylabel(f"u({dx * x}, t)")
+            plt.legend()
+            plt.show()
+
+
+def main(argv: list):
+    N: int = 100
+    k_max: int = 1000
+    eps: float = 1e-4
+    Laplace_solver(N, k_max, eps, 1, 5)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
